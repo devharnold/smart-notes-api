835053c6d9f199e2bc6133e1e08e6a66895a87b8
Henry Arnold <devharnold@gmail.com>
2025-09-29 00:59:28 +0300

    Write basic functions for the Users and Notes Repositories. Some basic Create and Read functions that are found within the repo layer.

--- internal/repositories/usersrepo.go
+++ internal/repositories/usersrepo.go
@@ -10,19 +10,23 @@ import (
 
 type UsersRepository struct{}
 
-func (r *UsersRepository) UploadUser(ctx context.Context, users models.Users) error {
+func (r *UsersRepository) UploadUser(ctx context.Context, user *models.User) (int64, error) {
 	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
 
-	insertQuery := "INSERT INTO users(user_email, first_name, last_name, password) VALUES ($1, $2, $3, $4)"
+	insertQuery := "INSERT INTO users(user_email, first_name, last_name, password) VALUES ($1, $2, $3, $4) RETURNING id"
 
 	// execute query
-	_, err := storage.Pool.Exec(ctx, insertQuery, users.UserEmail, users.FirstName, users.LastName, users.Password)
+	var userID int64
+	err := storage.Pool.QueryRow(ctx, insertQuery,
+		user.UserEmail,
+		user.FirstName,
+		user.LastName,
+	).Scan(&userID)
 	if err != nil {
-		return err
+		return 0, err
 	}
-
-	return nil
+	return userID, nil
 }
 
 func (r *UsersRepository) GetUserByEmail(ctx context.Context, email string) (models.User, error) {
@@ -34,6 +38,25 @@ func (r *UsersRepository) GetUserByEmail(ctx context.Context, email string) (mod
 	selectQuery := "SELECT * FROM users WHERE user_email = $1"
 
 	err := storage.Pool.QueryRow(ctx, selectQuery, email).Scan(
+		&user.UserEmail,
+		&user.FirstName,
+		&user.LastName,
+	)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func (r *UsersRepository) GetUserByID(ctx context.Context, id int64) (models.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	var user models.User
+
+	selectQuery := "SELECT * FROM users WHERE id = $1"
+
+	err := storage.Pool.QueryRow(ctx, selectQuery, id).Scan(
 		&user.UserEmail,
 		&user.FirstName,
 		&user.LastName,
